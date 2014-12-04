e0b6b6039d1f66b3eadadc08a8dfb1a2e3a9c074
Jigar Parekh <jigar.parekh@rishabhsoft.com>
2014-12-04 10:42:46 +0530

    completed plot 2

--- plot2.R
+++ plot2.R
@@ -1,8 +1,14 @@
 #unzip("exdata-data-household_power_consumption.zip")
+
 ## reads only data for 2007-02-01 and 2007-02-02
 data<-read.table(file="household_power_consumption.txt",header = T, sep = ";",na.strings = "?",skip=66636,nrows=2880)
 ## assigns column names 
 names(data) <- strsplit(readLines("household_power_consumption.txt",1),";",fixed=TRUE)[[1]]
-png("plot1.png", width=480, height=480, units = "px", bg = "transparent")
-hist(data$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+
+## converts character to date 
+data$datetime <- paste(data$Date,data$Time)
+data$datetime <- strptime(data$datetime,"%d/%m/%Y %H:%M:%S")
+
+png("plot2.png", width=480, height=480, units = "px", bg = "transparent")
+plot(data$datetime,data$Global_active_power,type="l",ylab = "Global Active Power (kilowatts)",xlab="")
 dev.off()
